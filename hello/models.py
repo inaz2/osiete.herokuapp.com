9a1974213dfe6fe82c2bde7e092ed2773fd5ac3d
inaz2 <inaz2@users.noreply.github.com>
2016-05-30 04:31:13 +0900

    fix models.py

--- hello/models.py
+++ hello/models.py
@@ -6,19 +6,19 @@ class Greeting(models.Model):
 
 
 class Topic(models.Model):
-    text = models.CharField(1024)
-    profile = models.CharField(256)
+    text = models.CharField(max_length=1024)
+    profile = models.CharField(max_length=256)
     ipaddress = models.GenericIPAddressField()
-    useragent = models.CharField(1024)
+    useragent = models.CharField(max_length=1024)
     created = models.DateTimeField('date created', auto_now_add=True)
     viewcount = models.IntegerField()
 
 
 class Answer(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    text = models.CharField(1024)
-    profile = models.CharField(256)
+    text = models.CharField(max_length=1024)
+    profile = models.CharField(max_length=256)
     ipaddress = models.GenericIPAddressField()
-    useragent = models.CharField(1024)
+    useragent = models.CharField(max_length=1024)
     created = models.DateTimeField('date created', auto_now_add=True)
     likecount = models.IntegerField()
